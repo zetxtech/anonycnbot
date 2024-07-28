1ccf52fa05b9ff1ed06e14ae2e82334ba249bfd9
jackzzs <jackzzs@outlook.com>
2024-07-29 04:25:45 +0800

    Update github url

--- anonyabbot/__init__.py
+++ anonyabbot/__init__.py
@@ -3,7 +3,7 @@ __author__ = {
     "jackzzs": "jackzzs@outlook.com",
 }
 __version__ = "1.0.0"
-__url__ = "https://github.com/anonyabbot/anonyabbot"
+__url__ = "https://github.com/zetxtech/anonyabbot"
 
 from .bot.father import FatherBot
 from .bot.group import GroupBot
